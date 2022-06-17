8270fa92713ff58a9524ef3f0ba492a7c0eb4b40
Standpoint <kelivdc@yahoo.com>
2022-06-17 21:48:52 +0700

    Update home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {   
+    container: {
+      center: true,
+      screens: {
+        md: '1040px',
+      }
+    },    
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
